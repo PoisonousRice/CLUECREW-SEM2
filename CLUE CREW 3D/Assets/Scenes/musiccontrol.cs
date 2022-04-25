9362d368f15308e68497a7fcd8c63fdb6437fffd
Petra <Pschuster2940@student.pps.net>
2022-04-25 13:13:45 -0700

    added music

---
+++ CLUE CREW 3D/Assets/Scenes/musiccontrol.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class musiccontrol : MonoBehaviour
+{
+    public static musiccontrol instance;
+
+        private void awake()
+    {
+
+        DontDestroyOnLoad(this.gameObject);
+
+        if (instance == null)
+        {
+            instance = this;
+
+        }
+
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    // Update is called once per frame
+   
+}
